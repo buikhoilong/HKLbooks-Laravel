7c27d3b3bc244ef20bc86eacbdd26c0f320f0d8d
Đỗ Quang Huy <doquanghuy12341@gamil.com>
2022-02-22 06:41:32 +0700

    update rate

--- routes/api.php
+++ routes/api.php
@@ -48,7 +48,7 @@ Route::prefix('/favourite')->group(function () {
     Route::delete('/deleteFav',[APIsController::class,'deleteFav']);
 });
 Route::prefix('/rates')->group(function () {
-    Route::get('/', [APIsController::class, 'getAllRateByBookId']);
+    Route::get('/getAllRateByBookId/{Id}', [APIsController::class, 'getAllRateByBookId']);
 });
 
 
